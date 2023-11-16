26fca379ae4feef17892f6f6061087dd8a26ae98
Falk Werner <falk.werner@gmx.net>
2023-11-16 20:56:06 +0000

    add example to create crl

--- README.md
+++ README.md
@@ -16,7 +16,7 @@ cmake --build build
 | [self_signed](src/self_signed.cpp) | Create a self signed certificate. |
 | [create_csr](src/create_csr.cpp) | Create a certificate signing request (CSR). |
 | [sign_csr](src/sign_csr.cpp) | Sign a CSR. |
-
+| [create_crl](src/create_crl.cpp) | Create certificate revokation list (CRL). |
 
 ### Create Self-Signed Certificate
 
@@ -129,6 +129,36 @@ Certificate:
 subject.pem: OK
 ```
 
+### Create Certificate Revokation List
+
+Prerequisites:
+- create a (self signed) certificate (`issuer.pem`) and private key (`issuer.key`)
+- create a CSR (`subject.csr`)
+- sign CSR (`subject.pem`)
+
+```
+> ./build/create_crl -f subject.crl -c subject.pem -i issuer.pem -k issuer.key
+
+> openssl crl -in subject.crl -text -noout
+Certificate Revocation List (CRL):
+        Version 2 (0x1)
+        Signature Algorithm: sha256WithRSAEncryption
+        Issuer: DC = org, DC = exampe, O = Example org, OU = Example Root CA, CN = Self Signed
+        Last Update: Nov 16 20:44:54 2023 GMT
+        Next Update: Nov 26 20:44:54 2023 GMT
+Revoked Certificates:
+    Serial Number: 01
+        Revocation Date: Nov 16 20:44:54 2023 GMT
+    Signature Algorithm: sha256WithRSAEncryption
+    Signature Value:
+        dc:48:9f:16:9d:95:ee:95:5a:e1:b3:59:fa:ba:83:16:a3:d4:
+        ...
+
+> openssl verify -CAfile issuer.pem -CRLfile subject.crl -crl_check subject.pem
+DC = org, DC = exampe, O = Example org, OU = Example CSR, CN = Req
+error 23 at 0 depth lookup: certificate revoked
+error subject.pem: verification failed
+```
 
 ## Depedencies
 
---
+++ src/create_crl.cpp
@@ -0,0 +1,232 @@
+#include <openssl/evp.h>
+#include <openssl/x509.h>
+#include <openssl/pem.h>
+
+#include <getopt.h>
+
+#include <ctime>
+#include <cstdlib>
+#include <cstdio>
+
+#include <string>
+#include <iostream>
+#include <filesystem>
+
+namespace
+{
+
+void print_usage()
+{
+    std::cout << R"(create_crl, (c) 2023 Falk Werner
+Creates a certificate revokation list (CRL).
+
+Usage:
+    create_crl [-f <filename>] [-i <filename>] [-k <filename>] [-c <filename>]
+
+Options:
+    -f, --filename <filename>    - name of the generated CRL file (default: crl.pem)
+    -i, --issuer   <filename>    - name of the issuers certificate (default: issuer.pem)
+    -k, --keyfile  <filename>    - name of the issuers private key (default: issuer.key)
+    -c, --certificate <filename> - name of certificate to revoke (default: cert.pem)
+
+Example:
+    creat_crl -f crl.pem
+)";
+}
+
+struct context
+{
+    context(int argc, char * argv[])
+    : show_help(false)
+    , exit_code(EXIT_SUCCESS)
+    , filename("crl.pem")
+    , certfile("cert.pem")
+    , issuer("issuer.pem")
+    , keyfile("issuer.key")
+    {
+        option const long_opts[] = {
+            {"filename", required_argument, nullptr, 'f'},
+            {"certificate", required_argument, nullptr, 'c'},
+            {"issuer", required_argument, nullptr, 'i'},
+            {"keyfile", required_argument, nullptr, 'k'},
+            {"help", no_argument, nullptr, 'h'},
+            {nullptr, 0, nullptr, 0}
+        };
+
+        opterr = 0;
+        optind = 0;
+
+        bool finished = false;
+        while (!finished)
+        {
+            int option_index = 0;
+            int const c = getopt_long(argc, argv, "f:c:k:i:h", long_opts, &option_index);
+            switch (c)
+            {
+                case -1:
+                    finished = true;
+                    break;
+                case 'f':
+                    filename = optarg;
+                    break;
+                case 'c':
+                    certfile = optarg;
+                    break;
+                case 'k':
+                    keyfile = optarg;
+                    break;
+                case 'i':
+                    issuer = optarg;
+                    break;
+                case 'h':
+                    show_help = true;
+                    finished = true;
+                    break;
+                default:
+                    std::cerr << "error: invalid command line option" << std::endl;
+                    exit_code = EXIT_FAILURE;
+                    show_help = true;
+                    finished = true;
+                    break;
+            }
+        }
+    }
+
+    bool show_help;
+    int exit_code;
+    std::string filename;
+    std::string certfile;
+    std::string issuer;
+    std::string keyfile;
+};
+
+void create_crl(
+    std::string const & filename,
+    std::string const & certfile,
+    std::string const & issuer,
+    std::string const & keyfile
+)
+{
+    X509_CRL * crl = nullptr;
+    FILE * file;
+
+    if (std::filesystem::is_regular_file(filename))
+    {
+        file = fopen(filename.c_str(), "rb");
+        if (nullptr != file)
+        {
+            PEM_read_X509_CRL(file, &crl, nullptr, nullptr);
+            fclose(file);
+        }
+        if (nullptr == file)
+        {
+            std::cerr << "error: failed to load CRL" << std::endl;
+            return;
+        }
+    }
+    else
+    {
+        crl = X509_CRL_new();        
+    }
+
+    X509 * issuer_cert = nullptr;
+    file = fopen(issuer.c_str(), "rb");
+    if (nullptr != file)
+    {
+        PEM_read_X509(file, &issuer_cert, nullptr, nullptr);
+        fclose(file);
+    }
+    if (nullptr == issuer_cert)
+    {
+        std::cerr << "error: failed to load issuer certificate" << std::endl;
+        X509_CRL_free(crl);
+        return;
+    }
+
+    EVP_PKEY * key = nullptr;
+    file = fopen(keyfile.c_str(), "rb");
+    if (nullptr != file)
+    {
+        PEM_read_PrivateKey(file, &key, nullptr, nullptr);
+        fclose(file);
+    }
+    if (nullptr == key)
+    {
+        std::cerr << "error: failed to load issuer key" << std::endl;
+        X509_free(issuer_cert);
+        X509_CRL_free(crl);
+        return;
+    }
+
+    X509 * cert = nullptr;
+    file = fopen(certfile.c_str(), "rb");
+    if (nullptr != file)
+    {
+        PEM_read_X509(file, &cert, nullptr, nullptr);
+        fclose(file);
+    }
+    if (nullptr == cert)
+    {
+        std::cerr << "error: failed to load certificate" << std::endl;
+        EVP_PKEY_free(key);
+        X509_free(issuer_cert);
+        X509_CRL_free(crl);
+        return;
+    }
+
+    X509_CRL_set_version(crl, X509_CRL_VERSION_2);
+    X509_CRL_set_issuer_name(crl, X509_get_issuer_name(issuer_cert));
+
+    ASN1_TIME * lastUpdate = ASN1_TIME_new();
+    X509_gmtime_adj(lastUpdate, 0);
+	X509_CRL_set_lastUpdate(crl, lastUpdate);
+    ASN1_TIME_free(lastUpdate);
+
+    ASN1_TIME * nextUpdate = ASN1_TIME_new();
+    X509_gmtime_adj(nextUpdate, 60 * 60 * 24 * 10);
+	X509_CRL_set_nextUpdate(crl, nextUpdate);
+    ASN1_TIME_free(nextUpdate);
+
+
+    X509_REVOKED * revoked = X509_REVOKED_new();
+    ASN1_TIME * revoked_at = ASN1_TIME_new();
+    X509_gmtime_adj(revoked_at, 0);
+    X509_REVOKED_set_revocationDate(revoked, revoked_at);
+    ASN1_TIME_free(revoked_at);
+
+    X509_REVOKED_set_serialNumber(revoked, X509_get_serialNumber(cert));
+    X509_CRL_add0_revoked(crl, revoked);
+
+    X509_CRL_sort(crl);
+    X509_CRL_sign(crl, key, EVP_sha256());
+
+    file = fopen(filename.c_str(), "wb");
+    if (nullptr != file)
+    {
+        PEM_write_X509_CRL(file, crl);
+        fclose(file);
+    }
+
+    X509_free(cert);
+    EVP_PKEY_free(key);
+    X509_free(issuer_cert);
+    X509_CRL_free(crl);
+}
+
+}
+
+int main(int argc, char * argv[])
+{
+    context ctx(argc, argv);
+    if (!ctx.show_help)
+    {
+        create_crl(ctx.filename, ctx.certfile, ctx.issuer, ctx.keyfile);
+    }
+    else
+    {
+        print_usage();
+    }
+
+    return ctx.exit_code;
+
+}
